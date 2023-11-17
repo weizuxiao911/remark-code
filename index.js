6d9c2002fd1cd10df0f5f0b49bd435379de10348
weizuxiao <727897965@qq.com>
2023-11-17 13:09:25 +0800

    处理打包

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-export {default} from './lib/index.js'
+/**
+ * @typedef {import('mdast').Root} Root
+ */
+
+import { code } from './syntax/syntax.js'
+import { codeFromMarkdown, codeToMarkdown } from './lib/util.js'
+
+/**
+ *  Create an extension to use in `unified`.
+ */
+export default function remarkCode() {
+  // @ts-expect-error: TS is wrong about `this`.
+  const self = /** @type {Processor} */ (this)
+  // const settings = options
+  const data = self.data()
+
+  const micromarkExtensions =
+    data.micromarkExtensions || (data.micromarkExtensions = [])
+  const fromMarkdownExtensions =
+    data.fromMarkdownExtensions || (data.fromMarkdownExtensions = [])
+  const toMarkdownExtensions =
+    data.toMarkdownExtensions || (data.toMarkdownExtensions = [])
+
+  micromarkExtensions.push(code())
+  fromMarkdownExtensions.push(codeFromMarkdown())
+  toMarkdownExtensions.push(codeToMarkdown())
+
+}
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Create an extension for `unified` to enable using in unified.
- */
-export default function remarkCode(): void;
-export type Root = import('mdast').Root;
